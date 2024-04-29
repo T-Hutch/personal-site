95ce9226f58716258254b741f68a1bb66883deba
Tom <thutch77@gmail.com>
2024-04-28 20:32:45 -0400

    Addition of Material Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatMenuModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'personal-site';
+
+  constructor(private router: Router) { }
+
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+    console.log(pageName);
+    
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import  routeConfig  from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routeConfig)
+    provideRouter(routeConfig), provideAnimationsAsync()
   ]
   })
   .catch(err => console.error(err));
